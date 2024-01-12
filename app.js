d8b28cffd33739152d38e9bb37acc89734dd1d75
mowuj <mowuj92@gmail.com>
2024-01-12 16:13:39 +0600

    review

--- app.js
+++ app.js
@@ -23,16 +23,12 @@ const displayService = (services) => {
     })
 }
 
-
-
 const loadDoctors = (search) => {
     document.getElementById("doctors").innerHTML = "";
     document.getElementById("spinner").style.display = "block";
-    console.log(search)
     fetch(`https://testing-8az5.onrender.com/doctor/list/?search=${search?search:""}`)
       .then((res) => res.json())
         .then((data) => {
-            console.log(data)
             if (data.results.length > 0) {
                 document.getElementById("spinner").style.display = "none";
                 document.getElementById("nodata").style.display = "none";
@@ -104,7 +100,30 @@ const handleSearch = () => {
     loadDoctors(value)
 }
 
+const loadReview = () => {
+    fetch("https://testing-8az5.onrender.com/doctor/review/")
+      .then((res) => res.json())
+      .then((data) => displayReview(data));
+}
+
+const displayReview = (reviews) => {
+    reviews.forEach((review) => {
+        const parent = document.getElementById("review-container");
+        const div = document.createElement("div");
+        div.classList.add("review-card");
+        div.innerHTML = `
+            <img src="./images/girl.png" alt="">
+            <h4>${review.reviewer}</h4>
+            <p>${review.body.slice(0,100)}</p>
+            <h6>${review.rating}</h6>
+        `;
+        parent.appendChild(div)
+        
+    })
+}
+
 loadServices();
 loadDoctors()
 loadDesignation()
 loadSpecialization()
+loadReview()
